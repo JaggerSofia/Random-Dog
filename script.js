335d68ef7a7e4a6e9f86f71c157d180247f1a377
Victoria Moore <victoriamoore563@gmail.com>
2019-10-15 17:33:00 -0700

    + functions 2 make button search 4 random dogs

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function getDogImage() {
+  fetch('https://dog.ceo/api/breeds/image/random/3')
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.dog-image').replaceWith(
+    `<img src="${responseJson.message}" class="dog-image">`
+  )
+  //display the results section
+  $('.result').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
